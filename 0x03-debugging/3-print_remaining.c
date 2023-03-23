a7fcbb428be4be4f3003c06582688b01926c5225
Nnamdi <okonkwonc@gmail.com>
2023-03-23 15:55:13 -0700

    Debugging

--- 0x03-debugging/3-print_remaining.c
+++ 0x03-debugging/3-print_remaining.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #include "main.h"
 
 /**
@@ -10,7 +9,6 @@
  * @year: year
  * Return: void
  */
-
 void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
